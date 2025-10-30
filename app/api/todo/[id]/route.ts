2531f38b1e73ed5314d80159bc1eb6174106840f
Vaideesh-siv <vaideesh.siv@gmail.com>
2025-10-30 18:27:39 -0500

    Fixed id error

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -2,7 +2,7 @@ import dbConnect from '../../../lib/dbConnect.js';
 import TodoModel from '../../../models/TodoModel.js';
 import { NextResponse, NextRequest } from 'next/server';
 
-export async function DELETE(req: Request, { params }: { params: {id: any}}) {
+export async function DELETE(req: Request, { params }: { params: {id: string}}) {
     await dbConnect();
 
     const { id } = await params
